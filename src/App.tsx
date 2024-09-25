6f7f16a9392d3c166e328b8c7aa8c5e01b704546
Alejandro Cárdenas Rodriguez <a@alecodev.com>
2024-09-25 01:30:17 -0500

    :bug: Fixed issue basename

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,26 @@ function AppRouter() {
     <BrowserRouter basename={BASE_URL}>
       <Routes>
         {PublicRoutes.map((item, id) => (
-          <Route key={id} path={item.path} element={<PublicRoute />}>
-            <Route key={id} path={item.path} element={<item.component />} />
+          <Route key={id} path={BASE_URL + item.path} element={<PublicRoute />}>
+            <Route
+              key={id}
+              path={BASE_URL + item.path}
+              element={<item.component />}
+            />
           </Route>
         ))}
 
         {PrivateRoutes.map((item, id) => (
-          <Route key={id} path={item.path} element={<PrivateRoute />}>
-            <Route key={id} path={item.path} element={<item.component />} />
+          <Route
+            key={id}
+            path={BASE_URL + item.path}
+            element={<PrivateRoute />}
+          >
+            <Route
+              key={id}
+              path={BASE_URL + item.path}
+              element={<item.component />}
+            />
           </Route>
         ))}
       </Routes>
